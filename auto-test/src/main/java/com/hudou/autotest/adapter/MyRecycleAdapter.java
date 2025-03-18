c44fee689b1d596180a28308eb4976de8980d339
liuyih <liuyih@newlandnpt.com>
2025-03-18 17:28:00 +0800

    测试项目按下优化

--- auto-test/src/main/java/com/hudou/autotest/adapter/MyRecycleAdapter.java
+++ auto-test/src/main/java/com/hudou/autotest/adapter/MyRecycleAdapter.java
@@ -1,10 +1,13 @@
 package com.hudou.autotest.adapter;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -20,8 +23,8 @@ import java.util.ArrayList;
 
 public class MyRecycleAdapter extends RecyclerView.Adapter<MyRecycleAdapter.RecycleViewHolder>{
     private OptionsFragment testCaseFragment = null;
-    private ArrayList<Item> itemList;
-    private FragmentActivity activity;
+    private final ArrayList<Item> itemList;
+    private final FragmentActivity activity;
     public MyRecycleAdapter(FragmentActivity activity, ArrayList<Item> itemList){
         this.activity = activity;
         this.itemList = itemList;
@@ -39,18 +42,26 @@ public class MyRecycleAdapter extends RecyclerView.Adapter<MyRecycleAdapter.Recy
         holder.tvItem.setText(itemList.get(position).getName());
         holder.tvDescription.setText(itemList.get(position).getDescription());
 
-        holder.imgBtnDetail.setOnClickListener(v -> {
-            testCaseFragment = new OptionsFragment(itemList.get(position).getClz());
-            activity.runOnUiThread(() -> {
-                FragmentManager supportFragmentManager = activity.getSupportFragmentManager();
-                supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_layout, testCaseFragment)
-                        .addToBackStack(testCaseFragment.getClass().getSimpleName())
-                        .commit();
-                supportFragmentManager.executePendingTransactions();
-
-            });
-        });
+        holder.llItemType.setOnClickListener(v -> enterOptionFragment(position));
+        holder.imgBtnDetail.setOnClickListener(v -> enterOptionFragment(position));
+//        holder.llItemType.setOnTouchListener((v, event) -> {
+//            switch (event.getAction()) {
+//                case MotionEvent.ACTION_DOWN:
+//                    // 按下时改变颜色
+//                    v.setBackgroundColor(Color.YELLOW);
+//                    return true; // 消费事件
+//                case MotionEvent.ACTION_UP:
+//                case MotionEvent.ACTION_CANCEL:
+//                    // 松开时恢复默认颜色
+//                    v.setBackgroundColor(Color.parseColor("#cfcfcf"));
+//                    if (v.performClick()){
+//                        enterOptionFragment(position);
+//                    }
+//                    return true; // 消费事件
+//                default:
+//                    return false;
+//            }
+//        });
     }
 
     @Override
@@ -59,15 +70,32 @@ public class MyRecycleAdapter extends RecyclerView.Adapter<MyRecycleAdapter.Recy
     }
 
     static class RecycleViewHolder extends RecyclerView.ViewHolder{
+        LinearLayout llItemType;
         TextView tvItem;
         TextView tvDescription;
         ImageButton imgBtnDetail;
         public RecycleViewHolder(@NonNull View itemView) {
             super(itemView);
-            tvItem = (TextView)itemView.findViewById(R.id.tv_item);
-            tvDescription = (TextView)itemView.findViewById(R.id.tv_description);
-            imgBtnDetail = (ImageButton) itemView.findViewById(R.id.imgbtn_detail);
+            llItemType = itemView.findViewById(R.id.ll_item_type);
+            tvItem = itemView.findViewById(R.id.tv_item);
+            tvDescription = itemView.findViewById(R.id.tv_description);
+            imgBtnDetail =  itemView.findViewById(R.id.imgbtn_detail);
         }
     }
+
+    private void enterOptionFragment(int position){
+        testCaseFragment = new OptionsFragment(itemList.get(position).getClz());
+        activity.runOnUiThread(() -> {
+            FragmentManager supportFragmentManager = activity.getSupportFragmentManager();
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_layout, testCaseFragment)
+                    .addToBackStack(testCaseFragment.getClass().getSimpleName())
+                    .commit();
+            supportFragmentManager.executePendingTransactions();
+
+        });
+    }
+
+
 
 }
