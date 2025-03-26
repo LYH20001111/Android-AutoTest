f35b8cbc3d67de3a40e856cfd05bd760f206480c
liuyih <liuyih@newlandnpt.com>
2025-03-26 15:35:40 +0800

    增加输出报告的功能2

--- auto-test/src/main/java/com/hudou/autotest/report/excel/ReportOutput.java
+++ auto-test/src/main/java/com/hudou/autotest/report/excel/ReportOutput.java
@@ -6,23 +6,32 @@ import android.os.Build;
 
 import com.hudou.autotest.base.activity.AutoTestMainActivity;
 import com.hudou.autotest.constant.ResultItem;
+import com.hudou.autotest.util.ReflectionUtils;
 
 import java.io.File;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.TreeMap;
 
 public class ReportOutput {
-    public static String excelPath;
-    private String[] titleName = {"案例测试项", "案例号", "测试结果", "中文案例描述", "英文案例描述", "案例详情"};
-    private String filePath = "/sdcard/auto_test/report/";
+    public static String excelPath = "还未输出测试报告";
+    private String filePath = ReflectionUtils.getConfig("reportPath");
+    private final static String excel2003L =".xls";    //2003- 版本的excel
+    private final static String excel2007U =".xlsx";  //2007版本
+    private final TreeMap<String, String[]> sheetMap = new TreeMap<String, String[]>(){{
+        put("测试案例结果汇总", new String[]{"案例测试项", "案例总数", "案例通过数", "案例失败数", "通过率", "开始时间", "结束时间", "总时长"});
+        put("测试案例结果详情", new String[]{"案例测试项", "案例号", "测试结果", "中文案例描述", "英文案例描述", "案例详情"});
+    }};
+
     public boolean outputExcel(){
         String time = ExcelUtils.stampToDate(String.valueOf(new Date().getTime()));
         File dir = new File(filePath);
         if (!dir.exists()){
             dir.mkdirs();
         }
-        filePath = filePath + File.separator + "TestReport_" + time + ".xlsx";
-        ExcelUtils.initExcel(filePath, titleName);
+        filePath = filePath + "TestReport_" + time + excel2007U;
+        ExcelUtils.initExcel(filePath, sheetMap);
         boolean isSuccess = ExcelUtils.writeDataToExcel(resultItemList, filePath, AutoTestMainActivity.getContext());
         if (isSuccess){
             excelPath = filePath;
